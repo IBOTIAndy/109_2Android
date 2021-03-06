56dc49dcaf37423ae2d0e14ce1806307304f6a8a
IBOTIAndy <36949276+IBOTIAndy@users.noreply.github.com>
2021-03-06 17:36:31 +0800

    make the HelloConstraint, portrait layout is computed.

--- app/src/main/java/com/bot/t105590006_hw/MainActivity.java
+++ app/src/main/java/com/bot/t105590006_hw/MainActivity.java
@@ -4,13 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCount=0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
         Log.i("MainActivity", "This is 109_2HW1 log statement. MainActivity layout is complete.");
+    }
+
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this, R.string.count_initial_value, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void resetCount(View view){
+        mCount = 0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+    public void addCount(View view){
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
