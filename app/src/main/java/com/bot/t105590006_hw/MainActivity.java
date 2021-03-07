297025dc0599db3f05c8b853255cabd9ea286f76
IBOTIAndy <36949276+IBOTIAndy@users.noreply.github.com>
2021-03-08 02:27:39 +0800

    add the change color from zero_button. add land, xlarge layout. add new color. computed HW2.

--- app/src/main/java/com/bot/t105590006_hw/MainActivity.java
+++ app/src/main/java/com/bot/t105590006_hw/MainActivity.java
@@ -11,28 +11,36 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int mCount=0;
     private TextView mShowCount;
+    private View zeroButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        zeroButton = findViewById(R.id.button_zero);
+        zeroButton.setBackgroundColor(getResources().getColor(R.color.darker_gray));
 
         Log.i("MainActivity", "This is 109_2HW1 log statement. MainActivity layout is complete.");
     }
 
+
     public void showToast(View view){
-        Toast toast = Toast.makeText(this, R.string.count_initial_value, Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, "toast count: " + mShowCount.getText().toString(), Toast.LENGTH_SHORT);
         toast.show();
     }
     public void resetCount(View view){
         mCount = 0;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+            zeroButton.setBackgroundColor(getResources().getColor(R.color.darker_gray));
+
     }
     public void addCount(View view){
         ++mCount;
-        if (mShowCount != null)
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
+            zeroButton.setBackgroundColor(getResources().getColor(R.color.purple_500));
+        }
     }
 }
