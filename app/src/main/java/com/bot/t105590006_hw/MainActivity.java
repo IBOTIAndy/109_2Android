150cb38f2ee62bffab5c22ee448a52adc200c7e4
IBOTIAndy <visua64@gmail.com>
2021-03-10 17:33:51 +0800

    add count_button have change color when text_number change to odd or even

--- app/src/main/java/com/bot/t105590006_hw/MainActivity.java
+++ app/src/main/java/com/bot/t105590006_hw/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private int mCount=0;
     private TextView mShowCount;
     private View zeroButton;
+    private View countButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
         zeroButton = findViewById(R.id.button_zero);
+        countButton = findViewById(R.id.button_count);
         zeroButton.setBackgroundColor(getResources().getColor(R.color.darker_gray));
 
         Log.i("MainActivity", "This is 109_2HW1 log statement. MainActivity layout is complete.");
@@ -31,16 +33,26 @@ public class MainActivity extends AppCompatActivity {
     }
     public void resetCount(View view){
         mCount = 0;
-        if (mShowCount != null)
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
             zeroButton.setBackgroundColor(getResources().getColor(R.color.darker_gray));
-
+            cheekText();
+        }
     }
     public void addCount(View view){
         ++mCount;
         if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
             zeroButton.setBackgroundColor(getResources().getColor(R.color.purple_500));
+            cheekText();
+        }
+    }
+    private void cheekText(){
+        if (mCount%2 == 1){ //odd
+            countButton.setBackgroundColor(getResources().getColor(R.color.teal_700));
+        }
+        else{   //even
+            countButton.setBackgroundColor(getResources().getColor(R.color.bule));
         }
     }
 }
