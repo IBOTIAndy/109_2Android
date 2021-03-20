852d1c09fbed0799c6b3634145cefc4143d4baa5
IBOTIAndy <36949276+IBOTIAndy@users.noreply.github.com>
2021-03-20 23:24:08 +0800

    02.2HW is ok. when rotate the device, the app state is preserved.

--- app/src/main/java/com/bot/t105590006_hw/MainActivity.java
+++ app/src/main/java/com/bot/t105590006_hw/MainActivity.java
@@ -6,26 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "Activity";
     private int mCount=0;
     private TextView mShowCount;
+    private EditText mEditText;
     private View zeroButton;
     private View countButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "------");
+        Log.d(LOG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.main_tview_count);
+        mEditText = findViewById(R.id.main_edit_text);
 //        zeroButton = findViewById(R.id.button_zero);
         countButton = findViewById(R.id.main_button_count);
 //        zeroButton.setBackgroundColor(getResources().getColor(R.color.darker_gray));
+        if(savedInstanceState != null) {
+            Log.d(LOG_TAG, "loading save instance.");
+            mShowCount.setText(savedInstanceState.getString("textViewCount"));
+            mCount = Integer.parseInt(mShowCount.getText().toString());
+            cheekText();
+            Log.d(LOG_TAG, "loading computed.");
+        }
+        Log.d(LOG_TAG, "MainActivity layout is complete.");
+    }
 
 
-        Log.i("MainActivity", "This is 109_2HW1 log statement. MainActivity layout is complete.");
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d("MainActivity", "This is 109_2HW04 log statement. Bundle");
+        super.onSaveInstanceState(outState);
+        outState.putString("textViewCount", mShowCount.getText().toString());
     }
 
 
@@ -65,5 +83,30 @@ public class MainActivity extends AppCompatActivity {
         else{   //even
             countButton.setBackgroundColor(getResources().getColor(R.color.bule));
         }
+    }
+    //查看各個狀態
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 }
