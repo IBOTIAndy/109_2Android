46bcb1b2823cc8d548cbdfad26a17c716280e756
IBOTIAndy <36949276+IBOTIAndy@users.noreply.github.com>
2021-03-21 17:08:01 +0800

    HW04 add implicit intents to open website, location, and camera. and shareCompat to share txt. HW04 is ok.

--- app/src/main/java/com/bot/t105590006_hw/MainActivity.java
+++ app/src/main/java/com/bot/t105590006_hw/MainActivity.java
@@ -1,9 +1,12 @@
 package com.bot.t105590006_hw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditText;
     private View zeroButton;
     private View countButton;
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 //        zeroButton = findViewById(R.id.button_zero);
         countButton = findViewById(R.id.main_button_count);
 //        zeroButton.setBackgroundColor(getResources().getColor(R.color.darker_gray));
+        mWebsiteEditText = findViewById(R.id.main_edit_uri);
+        mLocationEditText = findViewById(R.id.main_edit_loc);
+        mShareTextEditText = findViewById(R.id.main_edit_share);
         if(savedInstanceState != null) {
             Log.d(LOG_TAG, "loading save instance.");
             mShowCount.setText(savedInstanceState.getString("textViewCount"));
@@ -46,26 +55,57 @@ public class MainActivity extends AppCompatActivity {
         outState.putString("textViewCount", mShowCount.getText().toString());
     }
 
+    public void openWebsite(View view) { //從editText取得網址來開啟網頁
+        //從editText取得網址字串
+        String url = mWebsiteEditText.getText().toString();
+        //使用URI建立Intent
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
 
-    public void showToast(View view){
-        Toast toast = Toast.makeText(this, "toast count: " + mShowCount.getText().toString(), Toast.LENGTH_SHORT);
-        toast.show();
-    }
-    public void sayHello(View view){ //將畫面切換到helloToast
-        Intent intent = new Intent(this, helloToast.class); //指定要切換的activity給intent
-        Bundle bundle = new Bundle(); //使用bundle來傳遞資料
-        bundle.putString("count", Integer.toString(mCount)); //將count數字存入bundle內
-        intent.putExtras(bundle); //將bundle包進intent
-        startActivity(intent); //啟動intent建立Activity
-    }
-    public void resetCount(View view){
-        mCount = 0;
-        if (mShowCount != null) {
-            mShowCount.setText(Integer.toString(mCount));
-            zeroButton.setBackgroundColor(getResources().getColor(R.color.darker_gray));
-            cheekText();
+        //如果成功: 開啟；失敗: 回傳LOG-Debug
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("ImplicitIntents", "Can't handle this!");
         }
     }
+    public void openLocation(View view) { //從editText取得地址來開啟
+    //從editText取得地址字串
+        String loc = mLocationEditText.getText().toString();
+        //使用URI建立Intent
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc); //"geo:0,0?q=" -> 地址模式
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        //如果成功: 開啟；失敗: 回傳LOG-Debug
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
+    }
+    public void toCam(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //如果成功: 開啟；失敗: 回傳LOG-Debug
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("ImplicitIntents", "Can't open cam intent!");
+        }
+    }
+
     public void addCount(View view){
         ++mCount;
         if (mShowCount != null) {
