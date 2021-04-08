3ba67a63f8a8fada81112712d9688b578516495e
IBOTIAndy <36949276+IBOTIAndy@users.noreply.github.com>
2021-04-08 22:55:19 +0800

    layout computed.

---
+++ app/src/main/java/com/bot/t105590006_hw/Homework06.java
@@ -0,0 +1,14 @@
+package com.bot.t105590006_hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Homework06 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework06);
+    }
+}
